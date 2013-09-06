c9a1abc8a9ecdfa33728c09295da3ad19a8a9cfb
msilvius <msilvius37@gmail.com>
2013-09-05 17:46:11 -0700

    Rename EDMX

--- CathLab/CathLabData.Designer.cs
+++ CathLab/CathLabData.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'c:\users\lucas\documents\visual studio 2012\Projects\CathLab\CathLab\Model1.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Users\Mark\Documents\GitHub\Cath_Lab\CathLab\CathLabData.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
