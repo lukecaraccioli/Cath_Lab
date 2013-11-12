57b1e7e14b697db34c92f5e6bcfc18e150af3651
msilvius <msilvius37@gmail.com>
2013-11-11 21:46:02 -0800

    product entry

--- CathLab/Inventory/ProductEntry.aspx.cs
+++ CathLab/Inventory/ProductEntry.aspx.cs
@@ -32,7 +32,9 @@ namespace CathLab
                 else
                 {
                     //RadWindow rw = (RadWindow)Page.FindControl("");
-                    RadWindowManager.Windows.Add(rwNewEntry);                                                                
+                    RadWindowManager.Windows.Add(rwNewEntry);                    
+                        
+                    
                 }
             }
         }
@@ -58,7 +60,6 @@ namespace CathLab
                 prod.LotNumber = (txtLotNumber.Text != null) ? int.Parse(txtLotNumber.Text) : -1;
                 prod.ExpirationDate = rdpExpiration.SelectedDate;
                 prod.LocationID = int.Parse(lbxLoc.SelectedValue);
-                prod.StatusID = 4;  // 4 = 'Scanned'
                 context.Products.Add(prod);
                 context.SaveChanges();
             }
